b0aa1268c55bd81149d969164dac458b6dd51e38
Damjan032 <damjan0032@gmail.com>
2020-04-05 22:03:32 +0200

    init backend

---
+++ e_clinic-api/src/main/java/mrs/eclinicapi/EClinicApiApplication.java
@@ -0,0 +1,13 @@
+package mrs.eclinicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EClinicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EClinicApiApplication.class, args);
+    }
+
+}
---
+++ e_clinic-api/src/test/java/mrs/eclinicapi/EClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mrs.eclinicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EClinicApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
