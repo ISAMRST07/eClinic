67b7d356bb651c6fb0f1f3b2cb16f07c0601d75a
Alen <alenmujo10@gmail.com>
2020-04-09 16:25:58 +0200

    clinic room

---
+++ backend/src/main/java/mrs/eclinicapi/repository/ClinicRoomRepository.java
@@ -0,0 +1,13 @@
+package mrs.eclinicapi.repository;
+
+import java.util.List;
+
+import mrs.eclinicapi.model.ClinicRoom;
+
+public interface ClinicRoomRepository {
+    List<ClinicRoom> getAllClinicRooms();
+
+	void deleteById(int id);
+
+	ClinicRoom findById(int id);
+}
