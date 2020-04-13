b6ed1085c19a63f55116fa920a79d0e5d9993889
Damjan032 <34902687+Damjan032@users.noreply.github.com>
2020-04-13 18:17:27 +0200

    Zaboravio da dodama

---
+++ backend/src/main/java/mrs/eclinicapi/model/enums/InterventionType.java
@@ -0,0 +1,14 @@
+package mrs.eclinicapi.model.enums; /***********************************************************************
+ * Module:  InterventionType.java
+ * Author:  Manojlović
+ * Purpose: Defines the Class InterventionType
+ ***********************************************************************/
+
+/**
+ * @pdOid 54123b1c-f38c-496f-b5c7-0c61b9b0e951
+ */
+public enum InterventionType {
+    examination,
+    operation;
+
+}
---
+++ backend/src/main/java/mrs/eclinicapi/model/enums/UserType.java
@@ -0,0 +1,18 @@
+package mrs.eclinicapi.model.enums; /***********************************************************************
+ * Module:  UserType.java
+ * Author:  Manojlović
+ * Purpose: Defines the Class UserType
+ ***********************************************************************/
+
+/**
+ * @pdOid 98f01ff3-a11a-409a-bf3e-d05f81680e22
+ */
+public enum UserType {
+    unregistredUser,
+    clinicCentarAdministrator,
+    clinicAdministrator,
+    doctor,
+    nurse,
+    Patient;
+
+}
