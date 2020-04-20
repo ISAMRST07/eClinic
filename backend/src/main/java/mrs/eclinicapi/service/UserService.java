60a2800f71ea50bebf16f0dc15f488e1b47772b2
alen1010 <42214173+alen1010@users.noreply.github.com>
2020-04-20 16:45:56 +0200

    Add files via upload

--- backend/src/main/java/mrs/eclinicapi/service/UserService.java
+++ backend/src/main/java/mrs/eclinicapi/service/UserService.java
@@ -1,18 +1,32 @@
 package mrs.eclinicapi.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import mrs.eclinicapi.model.Clinic;
+import mrs.eclinicapi.model.User;
 import mrs.eclinicapi.repository.UserRepository;
 
 @Service
 public class UserService {
 
+	@Autowired
 	private UserRepository repository;
 	
-	@Autowired
-	public UserService(UserRepository repository) {
-		this.repository = repository;
+	public User addUser(User newUser){
+        return repository.save(newUser);
+    }
+	
+	public User findOne(Long id) {
+		return repository.findById(id).orElseGet(null);
+	}
+	public List<User> findAll() {
+		return repository.findAll();
 	}
 
+	public void remove(Long id) {
+		repository.deleteById(id);
+	}
 }
