0a5143d0db6f949af96472d07b2c042f21cbdc77
alen1010 <42214173+alen1010@users.noreply.github.com>
2020-05-11 16:50:54 +0200

    interventiontype

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,6 +5,7 @@ import ClinicAdministratorModules from "./ClinicAdministratorModules";
 import ClinicRoomModules from "./ClinicRoomModules";
 import DoctorModules from "./DoctorModules";
 import NurseModules from "./NurseModules";
+import InterventionTypeModule from "./InterventionTypeModule";
 import AuthModule from "./AuthModule";
 import MedicinesModules from "./MedicinesModules";
 import DiseaseModules from "./DiseaseModules";
@@ -13,16 +14,17 @@ import UnregisteredUsersModule from "./UnregisteredUsersModules";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    namespaced: true,
-    modules: {
-        clinics: ClinicModules,
-        clinicAdmins: ClinicAdministratorModules,
-        clinicRooms: ClinicRoomModules,
-        medicines: MedicinesModules,
-        doctor: DoctorModules,
-        nurse: NurseModules,
-        auth: AuthModule,
-        disease: DiseaseModules,
-        unregisteredUsers: UnregisteredUsersModule
-    }
+  namespaced: true,
+  modules: {
+    clinics: ClinicModules,
+    clinicAdmins: ClinicAdministratorModules,
+    clinicRooms: ClinicRoomModules,
+    medicines : MedicinesModules,
+    doctor: DoctorModules,
+    nurse: NurseModules,
+    auth: AuthModule,
+    disease: DiseaseModules,
+    unregisteredUsers: UnregisteredUsersModule,
+    interventionType : InterventionTypeModule
+  }
 })
