9260398e3b0f394e4411718da05fd1ceb679e684
alen1010 <42214173+alen1010@users.noreply.github.com>
2020-05-11 16:57:34 +0200

    interventiontype

--- backend/src/main/java/mrs/eclinicapi/model/InterventionType.java
+++ backend/src/main/java/mrs/eclinicapi/model/InterventionType.java
@@ -1,7 +1,6 @@
 package mrs.eclinicapi.model;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.voodoodyne.jackson.jsog.JSOGGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,8 +10,6 @@ import mrs.eclinicapi.generator.IdGenerator;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
-
 
 @Getter
 @Setter
@@ -20,30 +17,27 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor
 @JsonIdentityInfo(generator = JSOGGenerator.class)
-
-public class Intervention {
-
+public class InterventionType {
     @Id
     @Column(length=50)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "i_seq")
-    @GenericGenerator(name = "i_seq",
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "it_seq")
+    @GenericGenerator(name = "it_seq",
             strategy = "mrs.eclinicapi.generator.IdGenerator",
             parameters = {
-                    @org.hibernate.annotations.Parameter(name = IdGenerator.VALUE_PREFIX_PARAMETER, value = "I")})
+                    @org.hibernate.annotations.Parameter(name = IdGenerator.VALUE_PREFIX_PARAMETER, value = "IT")})
     private String id;
-    private LocalDateTime dateTime;
-
-    @ManyToOne
-    private ClinicRoom clinicRoom;
-
-    @ManyToOne
-    private Patient patient;
-
-    @ManyToOne
-    private Doctor doctor;
-
-    @OneToOne
-    private InterventionType interventionType;
     
-    private double duration;
+    private String name;
+    
+    @ManyToOne(cascade = {CascadeType.PERSIST}, fetch = FetchType.EAGER)
+    private Clinic clinic;
+    
+    private double price;
+
+	@Override
+	public String toString() {
+		return "InterventionType [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+    
+    
 }
