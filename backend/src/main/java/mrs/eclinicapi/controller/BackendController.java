daf5ec8b01c68d252842c102ca8c0c4707faebac
Haris Gegic <harry.gegic@gmail.com>
2020-06-03 15:36:55 +0200

    NEKE SITNE ISPRAVKE USER STORY 66

--- backend/src/main/java/mrs/eclinicapi/controller/BackendController.java
+++ backend/src/main/java/mrs/eclinicapi/controller/BackendController.java
@@ -1,26 +1,21 @@
 package mrs.eclinicapi.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.stream.Stream;
-
 @RestController
-public class BackendController {
+@RequestMapping(path = "/error")
+public class BackendController implements ErrorController {
 
-    @RequestMapping(path = "/{path:^(?!index\\.html|api).*}")
-    public ModelAndView forward(@PathVariable String path, HttpServletResponse response) {
+    @RequestMapping
+    public ModelAndView forward() {
         return new ModelAndView("forward:/");
     }
 
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
 }
