a6c4c3887384733a593978cb14d108792059e660
Haris Gegic <harry.gegic@gmail.com>
2020-06-14 23:13:37 +0200

    mala ispravka

--- backend/src/main/java/mrs/eclinicapi/EClinicApiApplication.java
+++ backend/src/main/java/mrs/eclinicapi/EClinicApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EClinicApiApplication {
 
-    public static final String APP_URL = "https://e-clinic-isamrs.herokuapp.com"; // za development
-
+    public static final String APP_URL = "https://e-clinic-isamrs.herokuapp.com"; // za deployment
+//    public static String appUrl = "http://localhost"; // za development
 
     public static void main(String[] args) {
         SpringApplication.run(EClinicApiApplication.class);
